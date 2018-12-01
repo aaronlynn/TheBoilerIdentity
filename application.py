e34f7147877a18ab004a34695a7f156257f1eca3
rileyturnbull <ribull44@gmail.com>
2018-12-01 16:49:52 -0500

    another example

--- application.py
+++ application.py
@@ -2,10 +2,10 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return 'Hello World!<a href="/home">Home</a>'
-
 @app.route("/home")
 def home():
 	return render_template('temphome.html')
+
+@app.route("/hello/<name>")
+def hello(name):
+	return render_template('hello.html', name=name)
