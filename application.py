f38c6e840452a2ac9b510e9947b58cdcbac84582
rileyturnbull <ribull44@gmail.com>
2018-12-01 17:09:43 -0500

    more examples

--- application.py
+++ application.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+
 app = Flask(__name__)
 
+@app.route("/")
 @app.route("/home")
 def home():
 	return render_template('temphome.html')
