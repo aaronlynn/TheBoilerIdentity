6d32c0cbf2e1f742689ad7eff39475bc1c8d106e
rileyturnbull <ribull44@gmail.com>
2018-12-02 15:45:21 -0500

    making forms work

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+import random
 import json
 from pusher import Pusher
 
@@ -16,6 +17,17 @@ pusher = Pusher(
 app = Flask(__name__)
 games = {}
 
+def generateRoomId():
+	alph = 'ABCDEFGHIJKLMNOPQRSTUVWYZ'
+	returned = ''
+	for i in range(0, 4):
+		returned += alph[random.randint(0, 25)]
+	while returned in games:
+		returned = ''
+		for i in range(0, 4):
+			returned += alph[random.randint(0, 25)]
+	return returned
+
 @app.route("/")
 @app.route("/home")
 def home():
@@ -27,14 +39,12 @@ def hello(name):
 
 @app.route("/newgame")
 def newgame():
-	if 'game' not in request.form or 'user' not in request.form:
+	if 'name' not in request.args:
 		return render_template('newgame.html', response='')
+	game = generateRoomId()
 
-	if request.form['game'] in games:
-		return render_template('newgame.html', response='Game already exists!')
-
-	games[request.form['game']] = {'players': [request.form['user']], 'owner': request.form['user']}
-	return render_template('lobby.html', game_id=request.form['game'], game=[request.form['user']], is_owner=True)
+	games[game] = {'players': [request.args['name']], 'owner': request.args['name']}
+	return render_template('lobby.html', game_id=game, game=json.dumps([request.args['name']]), is_owner=True)
 
 @app.route("/joingame")
 def joingame():
@@ -50,7 +60,7 @@ def joingame():
 		else:
 			games[game]['players'].append(username)
 			pusher.trigger(game, 'join-game', {'user': username})
-			return render_template('lobby.html', game_id=game, game=games[game], is_owner=False)
+			return render_template('lobby.html', game_id=game, game=json.dumps(games[game]), is_owner=False)
 	return render_template('joingame.html', found_game=game + ' does not exist!')
 
 @app.route("/game")
